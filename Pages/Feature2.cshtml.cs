cd256db8e7c9f179f7178c9e42fd4a13957af3da
ahmedosman2006 <ahmedrasmy.2006@gmail.com>
2025-08-23 23:22:59 +0300

    Feature 2

---
+++ Pages/Feature2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myopsapp.Pages
+{
+    public class Feature2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
